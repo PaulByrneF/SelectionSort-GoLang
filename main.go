5c4b158239dc64c3f7264dcbe7a0449cea6eba72
paul-byrne-nbi <paul.byrne@nbi.ie>
2022-02-24 03:07:19 +0000

    added bubblesort algorithm

--- main.go
+++ main.go
@@ -7,10 +7,41 @@ import (
 )
 
 func main() {
+
+	// Initialize Unsorted Slice
 	slice := generateSlice(24)
 	fmt.Printf(" -- Unsorted --\n%v\n\n", slice)
-	fmt.Printf(" -- Selection Sorted --\n%v\n", selectionSort(slice))
 
+	// Make copy of initial slice for selection sort algorithm
+	slice2 := make([]int, len(slice))
+	copy(slice2, slice)
+	fmt.Printf(" -- Selection Sort Algorithm --\n%v\n\n", selectionSort(slice2))
+
+	// Make copy of initial slice for bubblesort algorithm
+	slice3 := make([]int, len(slice))
+	copy(slice3, slice)
+	fmt.Printf(" -- Bubblesort Algorithm --\n%v\n\n", bubbleSort(slice3))
+}
+
+func bubbleSort(slice3 []int) []int {
+
+	length := len(slice3)
+	sorted := false
+
+	for !sorted {
+		swapped := false
+		for ind := 0; ind < length-1; ind++ {
+			if slice3[ind] > slice3[ind+1] {
+				slice3[ind+1], slice3[ind] = slice3[ind], slice3[ind+1]
+				swapped = true
+			}
+		}
+		if !swapped {
+			sorted = true
+		}
+		length = length - 1
+	}
+	return slice3
 }
 
 func generateSlice(size int) []int {
@@ -23,16 +54,15 @@ func generateSlice(size int) []int {
 }
 
 func selectionSort(slice []int) []int {
-	sliceCopy := slice[:]
-	var length = len(sliceCopy)
+	var length = len(slice)
 	for outerInd := 0; outerInd < length; outerInd++ {
 		var minIdx = outerInd
 		for innerInd := outerInd; innerInd < length; innerInd++ {
-			if sliceCopy[innerInd] < sliceCopy[minIdx] {
+			if slice[innerInd] < slice[minIdx] {
 				minIdx = innerInd
 			}
 		}
-		sliceCopy[outerInd], sliceCopy[minIdx] = sliceCopy[minIdx], sliceCopy[outerInd]
+		slice[outerInd], slice[minIdx] = slice[minIdx], slice[outerInd]
 	}
 	return slice
 }
